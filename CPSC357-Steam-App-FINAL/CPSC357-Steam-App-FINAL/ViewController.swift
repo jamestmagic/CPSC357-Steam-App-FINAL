80bf969796f25599ac47345ec5b15093fb65883e
kholland57 <42794325+kholland57@users.noreply.github.com>
2021-04-26 14:04:35 -0700

    First commit

---
+++ CPSC357-Steam-App-FINAL/CPSC357-Steam-App-FINAL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC357-Steam-App-FINAL
+//
+//  Created by Kelsey Holland on 4/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
