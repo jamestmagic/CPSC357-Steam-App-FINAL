21425aa6cee460e904a73b1ab14c41079b8276bd
kholland57 <42794325+kholland57@users.noreply.github.com>
2021-04-26 14:12:47 -0700

    Second Commit

--- CPSC357-Steam-App-FINAL/CPSC357-Steam-App-FINAL/ViewController.swift
+++ CPSC357-Steam-App-FINAL/CPSC357-Steam-App-FINAL/ViewController.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
+
+
+
+//View Controller Will be Main menu
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainMenuScroll: UIScrollView!
+    @IBOutlet weak var MainMenuViewController: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
